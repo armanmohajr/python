872f293bb7056674ea2dea98b782e9f540b3a2c6
Arman Ahmed <armanahmedmohamed@gmail.com>
2025-12-30 19:01:01 +0100

    second_commit

--- term-hinting-checking-validation.py
+++ term-hinting-checking-validation.py
@@ -1,15 +1,23 @@
-from typing import NewType, TypedDict
+import random
+from typing import NewType
+from dataclasses import dataclass
+
+import requests
+
+resp = requests.get("https://coreyms.com", timeout=5)
+status = resp.status_code
 
 RGB = NewType("RGB", tuple[int, int, int])
 HSL = NewType("HSL", tuple[int, int, int])
 
 
-class User(TypedDict):
+@dataclass
+class User:
     first_name: str
     last_name: str
     email: str
-    age: int | None
-    fav_color: RGB | None
+    age: int | None = None
+    fav_color: RGB | None = None
 
 
 def create_user(
@@ -20,16 +28,45 @@ def create_user(
 ) -> User:
     email = f"{first_name.lower()}_{last_name.lower()}@example.com"
 
-    return {
-        "first_name": first_name,
-        "last_name": last_name,
-        "email": email,
-        "age": age,
-        "fav_color": fav_color,
-    }
+    return User(
+        first_name=first_name,
+        last_name=last_name,
+        email=email,
+        age=age,
+        fav_color=fav_color
+    )
+
+
+def random_choice[T](items: list[T]) -> T:
+    return random_choice(items)
 
 
 user1 = create_user("John", "Doe", 20, fav_color=RGB((109, 123, 135)))
 user2 = create_user("arman", "ahmed", fav_color=RGB((109, 123, 135)))
 print(user1)
 print(user2)
+
+
+users = [user1, user2]
+random_user = random_choice(users)
+print(random_user)
+
+emails = [user.email for user in users]
+rando_email = random_choice(emails)
+print(rando_email)
+
+# def create_user(
+#     first_name: str,
+#     last_name: str,
+#     age: int | None = None,
+#     fav_color: RGB | None = None
+# ) -> User:
+#     email = f"{first_name.lower()}_{last_name.lower()}@example.com"
+
+#     return {
+#         "first_name": first_name,
+#         "last_name": last_name,
+#         "email": email,
+#         "age": age,
+#         "fav_color": fav_color,
+#     }
